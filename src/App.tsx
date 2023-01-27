8f7c987ca5ef55fb1e0297000fdf7ed8168aca50
Giacomo Rebonato <grebonato@outlook.ie>
2023-01-27 10:21:37 +0000

    github link

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,17 @@ function App() {
       style={{
         height: "100vh",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
       }}
     >
+      <a
+        target="_blank"
+        href="https://github.com/giacomorebonato/markdown-editor"
+      >
+        Github
+      </a>
       <MarkdownEditor value={text} onChange={setText} isDisabled={false} />
     </div>
   );
